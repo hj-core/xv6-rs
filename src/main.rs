866ded721accb34f75b5d196dbcb8810b460a908
hj-core <hj.forbetterfuture@gmail.com>
2024-12-18 22:33:09 +0800

    Config the minimal binary booted by qemu
    Can be passed to qemu as the kernel image.
    Use the process registers to perform simple arithmetic.
    
    (cherry picked from commit 0f39727cc731eb4b7f5fef145a65f4fc909b534f)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 #![no_std]
 #![no_main]
 
+use core::arch::asm;
 use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    unsafe { asm!("li s2, 5", "li s3, 7", "add s4, s2, s3", "mul s5, s3, s4",) }
     loop {}
 }
 
