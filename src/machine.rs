dd9686784e092e4c210f3c5babf2af36bbc4b2cb
hj-core <hj.forbetterfuture@gmail.com>
2024-12-20 00:47:02 +0800

    Config M-mode: set mstatus to S-mode

--- src/machine.rs
+++ src/machine.rs
@@ -6,3 +6,25 @@ pub const STACK_SIZE_PER_CPU: usize = 4096;
 pub fn store_mhartid_to_tp() {
     unsafe { asm!("csrr tp, mhartid") }
 }
+
+pub fn set_mstatus_to_s_mode() {
+    const MSTATUS_MPP_MASK: u64 = 3 << 11; // bit mask for mode bits
+    const MSTATUS_MPP_S: u64 = 1 << 11; // bits representing supervisor mode
+
+    let mut status = read_mstatus();
+    status &= !MSTATUS_MPP_MASK;
+    status |= MSTATUS_MPP_S;
+    write_mstatus(status);
+}
+
+fn read_mstatus() -> u64 {
+    let mut result: u64;
+    unsafe {
+        asm!("csrr {0}, mstatus", out(reg) result);
+    }
+    result
+}
+
+fn write_mstatus(value: u64) {
+    unsafe { asm!("csrw mstatus, {0}", in(reg) value) }
+}
