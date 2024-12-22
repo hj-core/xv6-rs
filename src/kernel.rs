512973a38abc6a3a1bb0bf2bd2ee216f376fd03f
hj-core <hj.forbetterfuture@gmail.com>
2024-12-22 22:02:35 +0800

    Print Hello World!

--- src/kernel.rs
+++ src/kernel.rs
@@ -1,8 +1,6 @@
 mod uart;
 
-use core::arch::asm;
-
 pub fn main() {
     uart::initialize();
-    unsafe { asm!("li a1, 1", "li a2, 2", "add a3, a1, a2", "mul a4, a2, a3") }
+    uart::busy_print("Hello World!");
 }
