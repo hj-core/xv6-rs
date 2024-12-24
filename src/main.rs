f13d7a3a8b68456453405ac98c8cbeac19d7c56a
hj-core <88603338+hj-core@users.noreply.github.com>
2024-12-24 10:34:59 +0800

    Set up the minimum binary that can be booted by qemu (#2)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::arch::{asm, global_asm};
+use core::panic::PanicInfo;
+
+// Entry point.
+// Must be placed at the address where qemu's -kernel jumps.
+// It just calls the _start.
+global_asm!(".section .text", ".global _entry", "_entry:", "call _start");
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    unsafe { asm!("li s2, 5", "li s3, 7", "add s4, s2, s3", "mul s5, s3, s4",) }
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
