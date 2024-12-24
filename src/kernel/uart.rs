38cec1061ee820916c975284d6ed828fd488501a
hj-core <hj.forbetterfuture@gmail.com>
2024-12-24 15:56:30 +0800

    Promote UART bit map const to mod level
    (cherry picked from commit 46361f01e73f0d5a932b19e96d3673e9a3aa6af8)

--- src/kernel/uart.rs
+++ src/kernel/uart.rs
@@ -1,4 +1,9 @@
 use crate::machine::uart;
+use crate::machine::uart::{
+    FCR_FIFO_ENABLE, FCR_RECEIVE_FIFO_RESET, FCR_TRANSMIT_FIFO_RESET,
+    IER_RECEIVER_READY_INTERRUPT_ENABLE, IER_TRANSMITTER_EMPTY_INTERRUPT_ENABLE,
+    LCR_8BIT_WORD_LENGTH, LCR_DIVISOR_LATCH_ENABLE, LSR_TRANSMIT_HOLDING_EMPTY,
+};
 
 pub fn initialize() {
     disable_all_interrupts();
@@ -13,25 +18,18 @@ fn disable_all_interrupts() {
 
 fn configure_lcr() {
     // Enable the Divisor Latch and set baud rate to 38_400
-    const LCR_DIVISOR_LATCH_ENABLE: u8 = 0x40;
     uart::LCR.write(LCR_DIVISOR_LATCH_ENABLE);
     uart::THR.write(0x03);
     uart::IER.write(0x00);
     // Disable the Divisor Latch; set the word length to 8 bits with no parity
-    const LCR_8BIT_WORD_LENGTH: u8 = 0x03;
     uart::LCR.write(LCR_8BIT_WORD_LENGTH);
 }
 
 fn reset_and_enable_fifo() {
-    const FCR_FIFO_ENABLE: u8 = 0x01;
-    const FCR_RECEIVE_FIFO_RESET: u8 = 0x02;
-    const FCR_TRANSMIT_FIFO_RESET: u8 = 0x04;
     uart::FCR.write(FCR_FIFO_ENABLE | FCR_RECEIVE_FIFO_RESET | FCR_TRANSMIT_FIFO_RESET);
 }
 
 fn enable_transmitter_and_receiver_interrupts_only() {
-    const IER_RECEIVER_READY_INTERRUPT_ENABLE: u8 = 0x01;
-    const IER_TRANSMITTER_EMPTY_INTERRUPT_ENABLE: u8 = 0x02;
     uart::IER.write(IER_RECEIVER_READY_INTERRUPT_ENABLE | IER_TRANSMITTER_EMPTY_INTERRUPT_ENABLE);
 }
 
@@ -43,6 +41,5 @@ pub fn busy_print(str: &str) {
 }
 
 fn is_thr_full() -> bool {
-    const LSR_TRANSMIT_HOLDING_EMPTY: u8 = 0x20;
     uart::LSR.read() & LSR_TRANSMIT_HOLDING_EMPTY == 0
 }
--- src/machine/uart.rs
+++ src/machine/uart.rs
@@ -1,8 +1,7 @@
-// The virt machine supports a NS16550 compatible UART.
-// See http://byterunner.com/16550.html for technical data.
+// 'virt' machine supports a NS16550 compatible UART.
 
 // The memory map address of UART0
-const VIRT_UART0_MEM_BASE: u64 = 0x1000_0000;
+const VIRT_UART0_MEMMAP_BASE: u64 = 0x1000_0000;
 
 pub struct Register {
     relative_addr: u64, // Relative to the base address
@@ -14,7 +13,7 @@ impl Register {
     }
 
     fn raw_ptr(&self) -> *mut u8 {
-        (VIRT_UART0_MEM_BASE + self.relative_addr) as *mut u8
+        (VIRT_UART0_MEMMAP_BASE + self.relative_addr) as *mut u8
     }
 
     pub fn write(&self, value: u8) {
@@ -22,8 +21,12 @@ impl Register {
     }
 }
 
-// The relative addresses of UART accessible registers.
+// Relative addresses of UART accessible registers.
 // Some addresses have different meanings depending on whether they are read or written.
+//
+// Ref:
+// TECHNICAL DATA ON 16550
+// http://byterunner.com/16550.html
 
 /// \[READ] Receive Holding Register
 pub const RHR: Register = Register { relative_addr: 0 };
@@ -33,21 +36,29 @@ pub const THR: Register = Register { relative_addr: 0 };
 
 /// \[WRITE] Interrupt Enable Register, or MSB of Divisor Latch when enabled
 pub const IER: Register = Register { relative_addr: 1 };
+pub const IER_RECEIVER_READY_INTERRUPT_ENABLE: u8 = 0x01;
+pub const IER_TRANSMITTER_EMPTY_INTERRUPT_ENABLE: u8 = 0x02;
 
 /// \[WRITE] FIFO control Register
 pub const FCR: Register = Register { relative_addr: 2 };
+pub const FCR_FIFO_ENABLE: u8 = 0x01;
+pub const FCR_RECEIVE_FIFO_RESET: u8 = 0x02;
+pub const FCR_TRANSMIT_FIFO_RESET: u8 = 0x04;
 
 /// \[READ] Interrupt Status Register
 pub const ISR: Register = Register { relative_addr: 2 };
 
 /// \[WRITE] Line Control Register
 pub const LCR: Register = Register { relative_addr: 3 };
+pub const LCR_DIVISOR_LATCH_ENABLE: u8 = 0x40;
+pub const LCR_8BIT_WORD_LENGTH: u8 = 0x03;
 
 /// \[WRITE] Modem Control Register
 pub const MCR: Register = Register { relative_addr: 4 };
 
 /// \[WRITE] Line Status Register
 pub const LSR: Register = Register { relative_addr: 5 };
+pub const LSR_TRANSMIT_HOLDING_EMPTY: u8 = 0x20;
 
 /// \[READ] Modem Status Register
 pub const MSR: Register = Register { relative_addr: 6 };
