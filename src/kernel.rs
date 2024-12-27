df86bdd17647f2be8c677f5fed11a8304d1d64ce
hj-core <hj.forbetterfuture@gmail.com>
2024-12-27 11:14:22 +0800

    Improve function names
    (cherry picked from commit 91d724fee8bea32879c191e683074b81ffc4a9db)

--- src/kernel.rs
+++ src/kernel.rs
@@ -9,19 +9,22 @@ mod uart;
 
 #[cfg(target_arch = "riscv64")]
 pub fn s_mode_initialize() {
-    enable_interrupt();
-    start_timer_interrupt();
+    configure_interrupt_enables();
+    schedule_timer_interrupt();
     uart::initialize();
     uart::busy_print_str("Hello World!\n");
 }
 
 #[cfg(target_arch = "riscv64")]
-fn enable_interrupt() {
+fn configure_interrupt_enables() {
+    // Configure the types of interrupts to receive
     riscv64::write_sie(riscv64::read_sie() | SIE_SEIE | SIE_STIE | SIE_SSIE);
 }
 
 #[cfg(target_arch = "riscv64")]
-fn start_timer_interrupt() {
+fn schedule_timer_interrupt() {
     // Ask for the very first timer interrupt
-    riscv64::write_stimecmp(riscv64::read_time() + 1_000_000)
+    riscv64::write_stimecmp(riscv64::read_time() + TIMER_INTERRUPT_INTERVAL)
 }
+
+const TIMER_INTERRUPT_INTERVAL: u64 = 1_000_000; // Tenth of a second
