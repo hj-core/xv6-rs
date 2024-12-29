c77da103885b8ffc7b2e01479a6a0f8e1314e602
hj-core <hj.forbetterfuture@gmail.com>
2024-12-29 18:36:49 +0800

    Add fun/busy_print_byte
    Rename original fun/busy_print to busy_print_str.
    
    (cherry picked from commit 4b027be8c03588bed17e82cb68e2f186c59cb18b)

--- src/kernel.rs
+++ src/kernel.rs
@@ -1,9 +1,8 @@
-mod uart;
-
 mod lock;
+mod uart;
 
 #[cfg(target_arch = "riscv64")]
 pub fn main() {
     uart::initialize();
-    uart::busy_print("Hello World!");
+    uart::busy_print_str("Hello World!\n");
 }
--- src/kernel/uart.rs
+++ src/kernel/uart.rs
@@ -1,9 +1,12 @@
+use crate::kernel::lock::Spinlock;
 use crate::machine::uart;
 use crate::machine::uart::{
     FCR_FIFO_ENABLE, FCR_RECEIVE_FIFO_RESET, FCR_TRANSMIT_FIFO_RESET,
     IER_RECEIVER_READY_INTERRUPT_ENABLE, IER_TRANSMITTER_EMPTY_INTERRUPT_ENABLE,
     LCR_8BIT_WORD_LENGTH, LCR_DIVISOR_LATCH_ENABLE, LSR_TRANSMIT_HOLDING_EMPTY,
 };
+
+static UART_LOCK: Spinlock = Spinlock::new();
 
 pub fn initialize() {
     disable_all_interrupts();
@@ -33,13 +36,25 @@ fn enable_transmitter_and_receiver_interrupts_only() {
     uart::IER.write(IER_RECEIVER_READY_INTERRUPT_ENABLE | IER_TRANSMITTER_EMPTY_INTERRUPT_ENABLE);
 }
 
-pub fn busy_print(str: &str) {
-    for &byte in str.as_bytes() {
-        while is_thr_full() {}
-        uart::THR.write(byte);
-    }
+fn print_byte(byte: u8) {
+    while is_thr_full() {}
+    uart::THR.write(byte);
 }
 
 fn is_thr_full() -> bool {
     uart::LSR.read() & LSR_TRANSMIT_HOLDING_EMPTY == 0
+}
+
+pub fn busy_print_byte(byte: u8) {
+    UART_LOCK.lock();
+    print_byte(byte);
+    UART_LOCK.unlock();
+}
+
+pub fn busy_print_str(str: &str) {
+    UART_LOCK.lock();
+    for &byte in str.as_bytes() {
+        print_byte(byte);
+    }
+    UART_LOCK.unlock();
 }
