7437f329f6e09bede33d87658b5953d1ba00dfdf
hj-core <88603338+hj-core@users.noreply.github.com>
2024-12-30 12:35:49 +0800

    Separate s mode initialization from m mode (#10)
    * Promote Riscv64 bit map const to mod level
    
    (cherry picked from commit 239edd4d5738348bb3d70952c669d8a38fa589b4)
    
    * Separate s mode initialization from m mode
    
    (cherry picked from commit 5379b753135c15c4a42a08a744336429f68521c7)
    
    * Improve function names
    
    (cherry picked from commit 91d724fee8bea32879c191e683074b81ffc4a9db)
    
    * Improve function name
    
    (cherry picked from commit 5775816223cad01e60ed338b96bd5e658dae327f)

--- src/kernel.rs
+++ src/kernel.rs
@@ -1,8 +1,30 @@
+#[cfg(target_arch = "riscv64")]
+use {
+    crate::machine::riscv64,
+    crate::machine::riscv64::{SIE_SEIE, SIE_SSIE, SIE_STIE},
+};
+
 mod lock;
 mod uart;
 
 #[cfg(target_arch = "riscv64")]
-pub fn main() {
+pub fn s_mode_initialize() {
+    configure_interrupt_enables();
+    schedule_timer_interrupt();
     uart::initialize();
     uart::busy_print_str("Hello World!\n");
 }
+
+#[cfg(target_arch = "riscv64")]
+fn configure_interrupt_enables() {
+    // Configure the types of interrupts to receive
+    riscv64::write_sie(riscv64::read_sie() | SIE_SEIE | SIE_STIE | SIE_SSIE);
+}
+
+#[cfg(target_arch = "riscv64")]
+fn schedule_timer_interrupt() {
+    // Ask for the very first timer interrupt
+    riscv64::write_stimecmp(riscv64::read_time() + TIMER_INTERRUPT_INTERVAL)
+}
+
+const TIMER_INTERRUPT_INTERVAL: u64 = 1_000_000; // Tenth of a second
