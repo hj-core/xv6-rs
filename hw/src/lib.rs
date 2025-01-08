a19ab7e9efbbc062f79504106db9af2787aca4b9
hj-core <hj.forbetterfuture@gmail.com>
2025-01-08 21:14:10 +0800

    Add const WORD_SIZE

--- hw/src/lib.rs
+++ hw/src/lib.rs
@@ -6,6 +6,7 @@ pub mod uart;
 
 use wrapper::{Address, Bytes};
 
+pub const WORD_SIZE: Bytes = Bytes(8);
 pub const MAX_CPUS: usize = 8;
 pub const DRAM_START: Address = Address(0x8000_0000);
 pub const DRAM_SIZE: Bytes = Bytes(128 << 20);
