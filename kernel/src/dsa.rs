b19505c15c210ff2a1e650760d3f4b5be4630eec
hj-core <hj.forbetterfuture@gmail.com>
2025-01-09 18:43:43 +0800

    Implement new for ListNode

--- kernel/src/dsa.rs
+++ kernel/src/dsa.rs
@@ -1,6 +1,16 @@
+use core::ptr::null_mut;
 use core::sync::atomic::AtomicPtr;
 
 pub struct ListNode {
     pub prev: AtomicPtr<ListNode>,
     pub next: AtomicPtr<ListNode>,
+}
+
+impl ListNode {
+    pub const fn new() -> Self {
+        Self {
+            prev: AtomicPtr::new(null_mut()),
+            next: AtomicPtr::new(null_mut()),
+        }
+    }
 }
