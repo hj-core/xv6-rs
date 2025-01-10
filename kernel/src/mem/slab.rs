30ad45acb58c6dbed9b437fc548b9dd7eae18036
hj-core <hj.forbetterfuture@gmail.com>
2025-01-10 21:58:04 +0800

    Implement Slab::initialize

--- kernel/src/mem/slab.rs
+++ kernel/src/mem/slab.rs
@@ -5,6 +5,8 @@
 use crate::dsa::ListNode;
 use crate::lock::Spinlock;
 use crate::HasHole;
+use core::ptr::null_mut;
+use core::sync::atomic::Ordering::Relaxed;
 use wrapper::{Address, Bytes};
 
 const CACHE_NAME_LENGTH: usize = 16;
@@ -43,6 +45,66 @@ struct Slab {
     slot0: Address,
     slot_size: Bytes,
     total_slots: usize,
+}
+
+impl Slab {
+    fn initialize<T>(&mut self, cache: &mut Cache)
+    where
+        T: Default + HasHole,
+    {
+        // Offset from the first slot's address to this slab's address.
+        let slot0_offset = Bytes(size_of::<Self>());
+        let slot_size = Bytes(size_of::<T>());
+
+        self.unlink_hole();
+        self.reset_used_bitmap_and_count();
+        self.set_slot0_and_total_slots(slot0_offset, cache.slab_size, slot_size);
+        self.install_objects::<T>(cache.hole(), slot_size);
+    }
+
+    fn unlink_hole(&mut self) {
+        self.hole().prev.store(null_mut(), Relaxed);
+        self.hole().next.store(null_mut(), Relaxed);
+    }
+
+    fn reset_used_bitmap_and_count(&mut self) {
+        for map in self.used_bitmap.iter_mut() {
+            *map = 0;
+        }
+        self.used_count = 0;
+    }
+
+    fn set_slot0_and_total_slots(
+        &mut self,
+        slot0_offset: Bytes,
+        slab_size: Bytes,
+        slot_size: Bytes,
+    ) {
+        let base_addr = Address::from(self as *mut Slab);
+        self.slot0 = base_addr + slot0_offset;
+
+        let slot_space = Bytes(slab_size.0 - slot0_offset.0);
+        self.total_slots = (slot_space.0 / slot_size.0).max(MAX_SLOTS_PER_SLAB);
+    }
+
+    /// For each slot, install the default value of struct [T];
+    /// Then, link its hole.[prev] to the [Cache] and its hole.[next] to this [Slab].
+    fn install_objects<T>(&mut self, cache_hole: &mut ListNode, slot_size: Bytes)
+    where
+        T: Default + HasHole,
+    {
+        let mut start = self.slot0;
+        for _ in 0..self.total_slots {
+            let mut object = T::default();
+            object.hole().prev.store(cache_hole, Relaxed);
+            object.hole().next.store(self.hole(), Relaxed);
+
+            unsafe {
+                (start.0 as *mut T).write(object);
+            }
+            start = start + slot_size;
+        }
+    }
 }
 
 impl HasHole for Slab {
