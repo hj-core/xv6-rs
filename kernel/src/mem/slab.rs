2f2f01babe912ecc76ef1484e2a86f03a4d93d65
hj-core <hj.forbetterfuture@gmail.com>
2025-04-13 21:36:16 +0800

    Pass test that general case holds invariants after allocation

--- kernel/src/mem/slab.rs
+++ kernel/src/mem/slab.rs
@@ -1971,6 +1971,65 @@ mod cache_tests {
     }
 
     #[test]
+    fn allocate_object_general_case_holds_cache_invariants() {
+        // Arrange:
+        // Create a cache that contains two empty slabs, two partial slabs and two full slabs.
+        type T = TestObject;
+        let layout = Layout::from_size_align(safe_slab_size::<T>(4), align_of::<SlabHeader<T>>())
+            .expect("Failed to create layout");
+
+        let mut cache = Cache::<T>::new(['c'; CACHE_NAME_LENGTH], layout);
+        let mut slab_man = SlabMan::<T>::new(layout);
+        let mut slab_objects = Vec::new();
+
+        let empty_slab1 = slab_man.new_test_slab(&raw mut cache);
+        let empty_slab2 = slab_man.new_test_slab(&raw mut cache);
+        unsafe {
+            (*empty_slab1).next = empty_slab2;
+            (*empty_slab2).prev = empty_slab1;
+        }
+        cache.slabs_empty = empty_slab1;
+
+        let partial_slab1 = slab_man.new_test_slab(&raw mut cache);
+        let partial_slab2 = slab_man.new_test_slab(&raw mut cache);
+        unsafe {
+            // Allocate some objects in each partial slab, leaving more than one free slot
+            for slab in [partial_slab1, partial_slab2] {
+                while (*slab).used_count < (*slab).total_slots - 2 {
+                    slab_objects
+                        .push(SlabHeader::allocate_object(slab).expect("Failed to allocate object"))
+                }
+            }
+            (*partial_slab1).next = partial_slab2;
+            (*partial_slab2).prev = partial_slab1;
+        }
+        cache.slabs_partial = partial_slab1;
+
+        let full_slab1 = slab_man.new_test_slab(&raw mut cache);
+        let full_slab2 = slab_man.new_test_slab(&raw mut cache);
+        unsafe {
+            // Fill each slab to make it full
+            for slab in [full_slab1, full_slab2] {
+                while !SlabHeader::is_full(slab) {
+                    slab_objects.push(
+                        SlabHeader::allocate_object(slab).expect("Failed to allocate object"),
+                    );
+                }
+            }
+            (*full_slab1).next = full_slab2;
+            (*full_slab2).prev = full_slab1;
+        }
+        cache.slabs_full = full_slab1;
+
+        // Act
+        let result = unsafe { Cache::allocate_object(&raw mut cache) };
+        assert!(result.is_ok(), "The result should be Ok but got {result:?}");
+
+        // Assert
+        unsafe { verify_cache_invariants(&raw mut cache) }
+    }
+
+    #[test]
     #[should_panic(expected = "`cache` should not be null")]
     fn allocate_from_empty_with_null_cache_should_panic() {
         type T = u64;
