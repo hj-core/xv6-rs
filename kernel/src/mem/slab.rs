06d58a747ec1fc97eb7195cb0ef3a27932fa8b1f
hj-core <hj.forbetterfuture@gmail.com>
2025-04-18 19:23:33 +0800

    Test full slabs only against different types

--- kernel/src/mem/slab.rs
+++ kernel/src/mem/slab.rs
@@ -1053,11 +1053,9 @@ mod cache_allocate_object_test {
         ),
     );
 
-    #[test]
-    fn full_slabs_only_returns_no_slab_available_err_cache_unmodified() {
+    fn test_full_slabs_only_returns_no_slab_available_err_cache_unmodified<T: Default + Debug>() {
         // Arrange:
         // Create a cache that contains only two full slabs.
-        type T = TestObject;
         let layout = Layout::from_size_align(safe_slab_size::<T>(2), align_of::<SlabHeader<T>>())
             .expect("Failed to create layout");
         let name = ['c'; CACHE_NAME_LENGTH];
@@ -1130,6 +1128,22 @@ mod cache_allocate_object_test {
             );
         }
     }
+
+    test_against_types!(
+        test_full_slabs_only_returns_no_slab_available_err_cache_unmodified,
+        (
+            full_slabs_only_returns_no_slab_available_err_cache_unmodified_for_type1,
+            Type1
+        ),
+        (
+            full_slabs_only_returns_no_slab_available_err_cache_unmodified_for_type2,
+            Type2
+        ),
+        (
+            full_slabs_only_returns_no_slab_available_err_cache_unmodified_for_type3,
+            Type3
+        ),
+    );
 
     #[test]
     fn slabs_empty_becomes_null_slabs_empty_is_null() {
